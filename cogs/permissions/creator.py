1d4ec29903f60315a08455f41e39a4343df247b4
aksu560 <hypernaurisukko@gmail.com>
2019-02-15 08:46:40 +0200

    Was lazy

--- cogs/permissions/creator.py
+++ cogs/permissions/creator.py
@@ -6,8 +6,8 @@ config.read(r"cogs\permissions\authorized_users.ini")
 
 
 def isCreator(ctx: commands.Context):
-    return ctx.message.author.id == config.get('creator', 'id')
+    return ctx.message.author.id == "114796980739244032"
 
 
 async def isCreatorAsync(ctx: commands.Context):
-    return ctx.message.author.id == config.get('creator', 'id')
+    return ctx.message.author.id == "114796980739244032"
