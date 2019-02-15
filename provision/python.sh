d75160de8e15c38df2d060306d6f94488f3b3593
aksu560 <hypernaurisukko@gmail.com>
2019-02-15 06:55:59 +0200

    Updated pip

--- provision/python.sh
+++ provision/python.sh
@@ -6,6 +6,7 @@ apt-get install -y python3-dev
 apt-get install -y python3-pip
 apt-get install -y git
 apt-get install -y libxml2
+pip3 install -U
 pip3 install asyncio
 pip3 install discord
 pip3 install urllib
