dd19cc922f83f44c9e72abbb185e6e14351ddbb2
aksu560 <hypernaurisukko@gmail.com>
2019-02-15 06:39:31 +0200

    foo

--- provision/python.sh
+++ provision/python.sh
@@ -4,8 +4,8 @@ apt-get update
 apt-get install -y python3.7
 apt-get install -y python3-pip
 apt-get install -y git
+apt-get install libxml2
 pip3 install asyncio
-pip3 install libxml2
 pip3 install discord
 pip3 install urllib
 pip3 install pyquery
