d00004db22c06f7db407dffae1fe1f8b5a9eb7f7
aksu560 <hypernaurisukko@gmail.com>
2019-02-15 07:12:59 +0200

    foo

--- provision/python.sh
+++ provision/python.sh
@@ -4,7 +4,7 @@ apt-get update
 apt-get install -y python 3.5.2
 apt-get install -y python3-pip
 apt-get install -y git
-sudo apt-get install libxml2-dev libxslt1-dev
+sudo apt-get install -y libxml2-dev libxslt1-dev
 pip3 install -U
 pip install lxml
 pip3 install asyncio
