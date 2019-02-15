c25bb32611ef3cac087fcb4dac2d2237442a35ee
aksu560 <hypernaurisukko@gmail.com>
2019-02-15 07:10:17 +0200

    more foo

--- provision/python.sh
+++ provision/python.sh
@@ -4,11 +4,11 @@ apt-get update
 apt-get install -y python 3.5.2
 apt-get install -y python3-pip
 apt-get install -y git
-apt-get install -y libxml2
+sudo apt-get install libxml2-dev libxslt1-dev
 pip3 install -U
+pip install lxml
 pip3 install asyncio
 pip3 install discord
-pip3 install urllib
 pip3 install pyquery
 
 cd /vagrant
