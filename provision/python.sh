7a61aadd845cad98e2f38841e3c633fcd938bb64
aksu560 <hypernaurisukko@gmail.com>
2019-02-16 15:06:39 +0200

    Nevermind

--- provision/python.sh
+++ provision/python.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 
-apt-get install -y python3-pip
-apt-get install -y git
-sudo apt-get install -y libxml2-dev libxslt1-dev
 pip3 install -U
 pip install lxml
 pip3 install asyncio
 pip3 install discord
 pip3 install pyquery
-
-cd /vagrant
-git pull
-python3 main.py
+pip3 install PIL
+pip3 install Pillow
