47e7b7c4841ebb76721539b958384f76c05601b4
aksu560 <hypernaurisukko@gmail.com>
2019-10-03 20:16:36 +0300

    changed logging level to something more sane

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 
 # Setting up logging as per discord.py's documentation
 logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.ERROR)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
